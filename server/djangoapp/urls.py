3b2363478a309e86efcfad79642831af7585e24b
omanaokar <omanaokar@gmail.com>
2023-07-14 17:32:35 -0400

    Second Commit
    Signed-off-by: omanaokar <omanaokar@gmail.com>

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,19 +10,20 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-    path(route='about', view=views.about, name='about'),
+    path(route='about/', view=views.about, name='about'),
     # path for contact us view
-    path(route='contact', view=views.contact, name='contact'),
+    path(route='contact/', view=views.contact, name='contact'),
     # path for registration
-
+    path(route='registration/', view=views.registration_request, name='registration'),
     # path for login
-
+    path('login/', views.login_request, name='login'),
     # path for logout
-
-    path(route='', view=views.get_dealerships, name='index'),
+    path('logout/', views.logout_request, name='logout'),
 
     # path for dealer reviews view
 
     # path for add a review view
+
+    path(route='', view=views.get_dealerships, name='index'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -31,16 +31,59 @@ def contact(request):
         return render(request, 'djangoapp/contact.html', context)
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['psw']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp/index.html')
+        else:
+            return render(request, 'djangoapp/index.html', context)
+            
+    else:
+        return render(request, 'djangoapp/index.html', context)
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    print("Log out the user `{}`".format(request.user.username))
+    logout(request)
+    return redirect('djangoapp/index.html')
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+def registration_request(request):
+    context = {}
+    # If it is a GET request, just render the registration page
+    if request.method == 'GET':
+        return render(request, 'djangoapp/registration.html', context)
+    # If it is a POST request
+    elif request.method == 'POST':
+        # Get user information from request.POST
+        username = request.POST.get('username', False)
+        password = request.POST.get('psw', False)
+        first_name = request.POST.get('firstname', False)
+        last_name = request.POST.get('lastname', False)
+        user_exist = False
+        try:
+            # Check if user already exists
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            # If not, simply log this is a new user
+            logger.debug("{} is new user".format(username))
+        # If it is a new user
+        if not user_exist:
+            # Create user in auth_user table
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password)
+            # Login the user and redirect to course list page
+            login(request, user)
+            return redirect("djangoapp/")
+        else:
+            return render(request, 'onlinecourse/registration.html', context)
+
+
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
